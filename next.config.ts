3222bd5e7e2dc50369e6b1da07ae833f81b0020c
Armen <armen.aghajanyan@softconstruct.com>
2026-06-03 18:40:30 +0400

    [FEAT]: fixed configs.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@
 const nextConfig = {
     output: 'export',
     distDir: 'build',
+    basePath: '/tile-expert-test-tasks',
+    assetPrefix: '/tile-expert-test-tasks/',
     images: {
         unoptimized: true,
     },
 };
+
 export default nextConfig;
